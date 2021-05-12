2fecbda6fcc100855c717e1c700b9e77db0b923c
michael sava <savamichael@hotmail.com>
2021-05-12 10:06:32 +0200

    Added Controllers for all Services to project

---
+++ HolidayMaker-API/HolidayMaker-API/Controllers/DestinationsController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HolidayMaker_API.Models;
+
+namespace HolidayMaker_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DestinationsController : ControllerBase
+    {
+        private readonly HolidayMakerContext _context;
+
+        public DestinationsController(HolidayMakerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Destinations
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Destination>>> GetDestinations()
+        {
+            return await _context.Destinations.ToListAsync();
+        }
+
+        // GET: api/Destinations/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Destination>> GetDestination(int id)
+        {
+            var destination = await _context.Destinations.FindAsync(id);
+
+            if (destination == null)
+            {
+                return NotFound();
+            }
+
+            return destination;
+        }
+
+        // PUT: api/Destinations/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDestination(int id, Destination destination)
+        {
+            if (id != destination.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(destination).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DestinationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Destinations
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Destination>> PostDestination(Destination destination)
+        {
+            _context.Destinations.Add(destination);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (DestinationExists(destination.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetDestination", new { id = destination.Id }, destination);
+        }
+
+        // DELETE: api/Destinations/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDestination(int id)
+        {
+            var destination = await _context.Destinations.FindAsync(id);
+            if (destination == null)
+            {
+                return NotFound();
+            }
+
+            _context.Destinations.Remove(destination);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DestinationExists(int id)
+        {
+            return _context.Destinations.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ HolidayMaker-API/HolidayMaker-API/Controllers/FavoriteHotelsController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HolidayMaker_API.Models;
+
+namespace HolidayMaker_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FavoriteHotelsController : ControllerBase
+    {
+        private readonly HolidayMakerContext _context;
+
+        public FavoriteHotelsController(HolidayMakerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/FavoriteHotels
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<FavoriteHotel>>> GetFavoriteHotels()
+        {
+            return await _context.FavoriteHotels.ToListAsync();
+        }
+
+        // GET: api/FavoriteHotels/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<FavoriteHotel>> GetFavoriteHotel(int id)
+        {
+            var favoriteHotel = await _context.FavoriteHotels.FindAsync(id);
+
+            if (favoriteHotel == null)
+            {
+                return NotFound();
+            }
+
+            return favoriteHotel;
+        }
+
+        // PUT: api/FavoriteHotels/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFavoriteHotel(int id, FavoriteHotel favoriteHotel)
+        {
+            if (id != favoriteHotel.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(favoriteHotel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FavoriteHotelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/FavoriteHotels
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<FavoriteHotel>> PostFavoriteHotel(FavoriteHotel favoriteHotel)
+        {
+            _context.FavoriteHotels.Add(favoriteHotel);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (FavoriteHotelExists(favoriteHotel.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetFavoriteHotel", new { id = favoriteHotel.Id }, favoriteHotel);
+        }
+
+        // DELETE: api/FavoriteHotels/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFavoriteHotel(int id)
+        {
+            var favoriteHotel = await _context.FavoriteHotels.FindAsync(id);
+            if (favoriteHotel == null)
+            {
+                return NotFound();
+            }
+
+            _context.FavoriteHotels.Remove(favoriteHotel);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool FavoriteHotelExists(int id)
+        {
+            return _context.FavoriteHotels.Any(e => e.Id == id);
+        }
+    }
+}
