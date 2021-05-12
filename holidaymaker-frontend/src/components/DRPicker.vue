e0403426224d125ceeec594ebb1e39276ee20370
giddster <73110741+giddster@users.noreply.github.com>
2021-05-12 22:59:46 +0200

    moved bootstrap reference
    back to how it looked before but no overlap between buttons. datepicker dropdowns are not responsive to window sizing yet though

--- holidaymaker-frontend/src/components/DRPicker.vue
+++ holidaymaker-frontend/src/components/DRPicker.vue
@@ -6,20 +6,21 @@
 				<div class="col-md-4" id="checkInDiv">
                     <button class="btn btn-primary btn-md dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="checkInDateButton">
                         <i class="far fa-calendar-check" id="checkInIcon"></i>
-                        Checkin date
+                        <p>Checkin date</p>
                     </button>
 				</div>
 
 				<div class="col-md-4" id="checkOutDiv">
                     <button class="btn btn-primary btn-md dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="checkOutDateButton">
                         <i class="far fa-calendar-times" id="checkOutIcon"></i>
-                        Checkout date
+                        <p> Checkout date</p>
                     </button>
 				</div>
                 
 				<div class="col-md-4" id="guestsButtonDiv">
                     <button type="button" class="btn btn-info btn-md" id="guestsButton">
-                        <i class="fas fa-users" id="guestsIcon"> Guests</i> 
+                        <i class="fas fa-users" id="guestsIcon"></i>
+						<p> Guests</p> 
                     </button>
 				</div>
 			</div>
