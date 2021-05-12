ed71bdb06eb930729ccd540f538611a173bf4dcf
giddster <73110741+giddster@users.noreply.github.com>
2021-05-12 23:08:16 +0200

    minor edits
    small design fixes on datepicker dropdowns

--- holidaymaker-frontend/src/App.vue
+++ holidaymaker-frontend/src/App.vue
@@ -2,7 +2,7 @@
   <div id="nav">
     <Header/>
     <Search />
-    <img src="https://pix10.agoda.net/hotelImages/124/1246280/1246280_16061017110043391702.jpg?s=1024x768" style="padding:10px;" />
+    <img src="https://pix10.agoda.net/hotelImages/124/1246280/1246280_16061017110043391702.jpg?s=1024x768" style="padding:20px;" />
     <Footer />
   </div>
   <router-view />
