5206ac35b1e8415fedb602f4f4fdf267c6aaae24
giddster <73110741+giddster@users.noreply.github.com>
2021-05-13 12:03:15 +0200

    fixed bootstrap
    hopefully, tested with a modal on guests button and it works (at least for now)

--- holidaymaker-frontend/src/components/DRPicker.vue
+++ holidaymaker-frontend/src/components/DRPicker.vue
@@ -18,14 +18,38 @@
 				</div>
                 
 				<div class="col-md-4" id="guestsButtonDiv">
-                    <button type="button" class="btn btn-info btn-md" id="guestsButton">
+                    <button type="button" class="btn btn-info btn-md" data-toggle="modal" data-target="#guestsModal" id="guestsButton">
                         <i class="fas fa-users" id="guestsIcon"></i>
 						<p> Guests</p> 
                     </button>
+
+					<!-- Modal -->
+					<div class="modal fade" id="guestsModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+					<div class="modal-dialog">
+						<div class="modal-content">
+						<div class="modal-header">
+							<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+							</button>
+						</div>
+						<div class="modal-body">
+							...
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+							<button type="button" class="btn btn-primary">Save changes</button>
+						</div>
+						</div>
+					</div>
+					</div>
+
+
+
+
 				</div>
 			</div>
 		</div>
 	</div>
 </div>
-
 </template>
