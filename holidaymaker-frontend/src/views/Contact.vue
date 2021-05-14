46f028f3874f895af1e75f895ec310c7f14c7faf
giddster <73110741+giddster@users.noreply.github.com>
2021-05-14 16:44:54 +0200

    finished content views
    still need a bit of styling though

--- holidaymaker-frontend/src/views/Contact.vue
+++ holidaymaker-frontend/src/views/Contact.vue
@@ -52,7 +52,7 @@
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle">What's new, pussycat?</h5>
+                <h5 class="modal-title" id="exampleModalLongTitle">What do you want?!</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -97,9 +97,6 @@
                                             </p>
 
                                         </div>
-
-
-
                                     </div>
                                 </div>
                             </div>
