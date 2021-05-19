f404c972be8d8158a093e0ec44204898a4631716
giddster <73110741+giddster@users.noreply.github.com>
2021-05-19 13:49:53 +0200

    Update Header.vue
    temporary test button for http get

--- holidaymaker-frontend/src/components/Header.vue
+++ holidaymaker-frontend/src/components/Header.vue
@@ -12,7 +12,7 @@
 				<div class="col-md-4" id="middleHeaderDiv">
 				</div>
 				<div class="col-md-4" id="rightHeaderDiv">
-					<button @click="fetchAllDestinations">bajs</button>
+					<button @click="fetchAllDestinations">Click me for GET</button>
 					<ProfileDropdown />
 				</div>
 			</div>
