4b0dbf9089881bd2072903f96dd73da11a17283c
giddster <73110741+giddster@users.noreply.github.com>
2021-05-19 16:02:23 +0200

    Update SearchResults.vue
    made small todo-list on what needs to be inside this view (which will have at least one component - the filter component - inside of it)

--- holidaymaker-frontend/src/views/SearchResults.vue
+++ holidaymaker-frontend/src/views/SearchResults.vue
@@ -1,3 +1,10 @@
 <template>
-bajs i byxan
+- return available hotels based on given date and amount of guests (iterate with v-for)
+    -in available hotels:
+            - icons for bools
+            - review average
+            - star rating
+            - thumbnail pic
+- "book now" button for each hotel option
+- filter component
 </template>
