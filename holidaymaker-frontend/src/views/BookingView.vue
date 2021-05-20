4b7463de7b82bea084f82575341b49689481f1d8
Armin Ljajic <arminljajic@hotmail.com>
2021-05-20 15:15:12 +0200

    Added filtering options and created component

--- holidaymaker-frontend/src/views/BookingView.vue
+++ holidaymaker-frontend/src/views/BookingView.vue
@@ -3,6 +3,7 @@
     <booking-user-details></booking-user-details>
     <hotel-details></hotel-details>
     <room-details></room-details>
+    <filter-component></filter-component>
 </template>
 
 
@@ -10,12 +11,14 @@
 import BookingUserDetails from '../components/BookingUserDetails.vue'
 import HotelDetails from '../components/HotelDetails.vue'
 import RoomDetails from '../components/RoomDetails.vue'
+import FilterComponent from '../components/FilterComponent.vue'
 export default {
   components: { 
       
     BookingUserDetails,
     HotelDetails,
-    RoomDetails
+    RoomDetails,
+    FilterComponent
   
   },
     
