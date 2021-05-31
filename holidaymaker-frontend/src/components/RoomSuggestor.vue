78ea4eb628437b407d698b0715182cc65869f0ff
Armin Ljajic <arminljajic@hotmail.com>
2021-05-31 16:02:22 +0200

    changed component position

--- holidaymaker-frontend/src/components/RoomSuggestor.vue
+++ holidaymaker-frontend/src/components/RoomSuggestor.vue
@@ -2,10 +2,9 @@
     <div class="card">
         <h3>Classic double room</h3>
             <img v-bind:src="img">
-        <div class="form">
-            <p>Options: </p>
-            <button class="btn btn-primary">Book now</button>
-        </div>
+            <div class="box">
+                <button class="btn btn-primary">Book now</button>
+            </div>
     </div>
 </template>
 
@@ -21,35 +20,40 @@ export default {
 
 <style scoped>
 .card {
+  background: rgb(246, 246, 252);
+  position: relative;
+  right: 800px;
   border-radius: 12px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   padding: 1rem;
   margin: 2rem auto;
-  max-width: 100rem;
+  max-width:70rem;
+  
 }
-.form{
-    overflow:hidden;
-    position:relative;
-    left:250px;
-    bottom: 80px;
-    border-style: solid;
-    border-width: 1px;
-    width: fit-content;
-    padding: 1rem;
-
-}
+/* .box {
+    margin:0 auto; 
+    width:600px;
+    padding:2px;
+    background:#f9f9f9;
+    border:2px solid #333;
+} */
 img {
+    overflow: hidden;
     position:relative;
-    top: 30px;
     max-width: 200px;
 }
 h3{
     position: relative;
-    top:30px;
     font-size: 15px;
     margin-left: 30px;
 }
 button{
     margin-left: 30rem;
+    background: lightcoral;
+    border: 1px solid lightcoral;
+}
+button:hover{
+    background: rgb(246, 114, 114);
+    border: 1px solid lightcoral;
 }
 </style>
