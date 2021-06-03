b14e49a844c5b172e5a55726d03113ad0204bef7
Daniel <danielriddersporre@gmail.com>
2021-06-03 18:25:22 +0200

    Image Cropping RoomSuggestor
    Finished the roomimage cropping and made minor changes to the "cards" showing the rooms.

--- holidaymaker-frontend/src/components/RoomSuggestor.vue
+++ holidaymaker-frontend/src/components/RoomSuggestor.vue
@@ -1,14 +1,16 @@
 <template>
     <div class="room-box" v-for="room in filteredRooms" :key="room.id">
-        <h5>Type: {{ room.roomType.typeName }}</h5>
-            <img :src='roomImage(room.hotelId, room.roomTypeId)'>
-            <div class="box">
-                <p><i class="fas fa-hashtag"></i><i>Room number: {{ room.roomNo}} </i> </p>
-                <p><i class="fas fa-tag"></i> {{ room.roomType.price }} SEK/night </p>
-                <p> <i class="fas fa-users"></i> {{ room.roomType.capacity }} persons </p>
-                <p><i> <i class="fas fa-bed"></i> {{ room.noOfSpareBeds }} spare bed(s) </i></p>
-                <button class="btn btn-primary">Add to booking</button>
-            </div>
+        <div class="room-box-1">
+            <h5>{{ room.roomType.typeName }}</h5>
+            <img :src='roomImage(room.hotelId, room.roomTypeId)' class="room-image-container">
+        </div>
+        <div class="room-box-2">
+            <p><i class="fas fa-hashtag"></i><i>Room number: {{ room.roomNo}} </i> </p>
+            <p><i class="fas fa-tag"></i> {{ room.roomType.price }} SEK/night </p>
+            <p> <i class="fas fa-users"></i> {{ room.roomType.capacity }} persons </p>
+            <p><i> <i class="fas fa-bed"></i> {{ room.noOfSpareBeds }} spare bed(s) </i></p>
+            <button class="btn btn-primary room-booking">Add to booking</button>
+        </div>
     </div>
 </template>
 
@@ -45,25 +47,34 @@ export default {
 <style scoped>
 
 .room-box {
-  background: rgb(246, 246, 252);
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 20px;
-  margin: 20px auto;
-  width: 95%;
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    grid-template-rows: 1fr;
+    grid-column-gap: 0px;
+    grid-row-gap: 0px; 
+    background: rgb(246, 246, 252);
+    border-radius: 12px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    padding: 20px;
+    margin: 20px auto;
+    width: 95%;
 }
 
-img {
-    overflow: hidden;
-    max-width: 200px;
+.room-box-1 {
+    width: 100%;
 }
 
-button{
-    background: lightcoral;
-    border: 1px solid lightcoral;
+.room-box-2 {
+    width: 100%;
+    padding-left: 30px;
+    padding-top: 30px;
 }
-button:hover{
-    background: rgb(246, 114, 114);
-    border: 1px solid lightcoral;
+
+.room-image-container {
+    width: 400px;
+    height: 200px;
+    float: right;
+    object-fit: cover;
+    border: 1px solid darkgrey
 }
 </style>
