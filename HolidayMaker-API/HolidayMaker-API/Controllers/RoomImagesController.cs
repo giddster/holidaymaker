ef746c459ba08e7b30426b7999cd1cc59db403d8
Daniel <danielriddersporre@gmail.com>
2021-06-04 16:22:52 +0200

    Created new controllers
    Created controllers for getting room- and hotelimages.

---
+++ HolidayMaker-API/HolidayMaker-API/Controllers/RoomImagesController.cs
@@ -0,0 +1,29 @@
+﻿using HolidayMaker_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HolidayMaker_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomImagesController : Controller
+    {
+        private readonly HolidayMakerContext _context;
+
+        public RoomImagesController(HolidayMakerContext context)
+        {
+            _context = context;
+        }
+
+        //GET: api/RoomImages
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<RoomImage>>> GetRoomImages()
+        {
+            return await _context.RoomImages.ToListAsync();
+        }
+    }
+}
