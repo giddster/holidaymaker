7cfaa0f69a534279b90f0f04bd109db5b39ab5e7
michael sava <savamichael@hotmail.com>
2021-06-04 20:24:25 +0200

    Added endpoints for hotelImages and to include room images in hotel query

--- HolidayMaker-API/HolidayMaker-API/Controllers/HotelsController.cs
+++ HolidayMaker-API/HolidayMaker-API/Controllers/HotelsController.cs
@@ -1,12 +1,11 @@
-﻿using System;
+﻿using HolidayMaker_API.Models;
+using HolidayMaker_API.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using HolidayMaker_API.Models;
-using HolidayMaker_API.Services;
 
 
 namespace HolidayMaker_API.Controllers
@@ -31,7 +30,7 @@ namespace HolidayMaker_API.Controllers
             return await _context.Hotels.ToListAsync();
         }
 
-        
+
         // GET: api/Hotels/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Hotel>> GetHotel(int id)
@@ -46,131 +45,239 @@ namespace HolidayMaker_API.Controllers
             return hotel;
         }
 
-        // PUT: api/Hotels/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutHotel(int id, Hotel hotel)
-        {
-            if (id != hotel.Id)
-            {
-                return BadRequest();
-            }
 
-            _context.Entry(hotel).State = EntityState.Modified;
+        #region Behövs ej kan raderas
+        //// PUT: api/Hotels/5
+        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutHotel(int id, Hotel hotel)
+        //{
+        //    if (id != hotel.Id)
+        //    {
+        //        return BadRequest();
+        //    }
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!HotelExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //    _context.Entry(hotel).State = EntityState.Modified;
 
-            return NoContent();
-        }
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!HotelExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
 
-        // POST: api/Hotels
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Hotel>> PostHotel(Hotel hotel)
-        {
-            _context.Hotels.Add(hotel);
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateException)
-            {
-                if (HotelExists(hotel.Id))
-                {
-                    return Conflict();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //    return NoContent();
+        //}
 
-            return CreatedAtAction("GetHotel", new { id = hotel.Id }, hotel);
-        }
+        //// POST: api/Hotels
+        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //[HttpPost]
+        //public async Task<ActionResult<Hotel>> PostHotel(Hotel hotel)
+        //{
+        //    _context.Hotels.Add(hotel);
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateException)
+        //    {
+        //        if (HotelExists(hotel.Id))
+        //        {
+        //            return Conflict();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
 
-        // DELETE: api/Hotels/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteHotel(int id)
-        {
-            var hotel = await _context.Hotels.FindAsync(id);
-            if (hotel == null)
-            {
-                return NotFound();
-            }
+        //    return CreatedAtAction("GetHotel", new { id = hotel.Id }, hotel);
+        //}
 
-            _context.Hotels.Remove(hotel);
-            await _context.SaveChangesAsync();
+        //// DELETE: api/Hotels/5
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> DeleteHotel(int id)
+        //{
+        //    var hotel = await _context.Hotels.FindAsync(id);
 
-            return NoContent();
-        }
+        //    if (hotel == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-        private bool HotelExists(int id)
-        {
-            return _context.Hotels.Any(e => e.Id == id);
-        }
+        //    _context.Hotels.Remove(hotel);
+        //    await _context.SaveChangesAsync();
+
+        //    return NoContent();
+        //}
+
+        //private bool HotelExists(int id)
+        //{
+        //    return _context.Hotels.Any(e => e.Id == id);
+        //}
+        #endregion
+
+
+
+        #region OLD Before changes
+        //[HttpGet("/api/allHotelsByDestinationId/{destinationId}")]
+
+        //public async Task<IEnumerable<Hotel>> GetAllHotelsByDestinationId(int destinationId)
+
+        //{
+
+        ////Maybe add some verification controls
+
+
+        //var availableHotels = await _hotelService.GetAllHotelsByDestination(destinationId);
+
+
+        //return availableHotels;
+
+
+        //}
+        #endregion
 
 
         [HttpGet("/api/allHotelsByDestinationId/{destinationId}")]
-        public async Task<IEnumerable<Hotel>> GetAllHotelsByDestinationId(int destinationId)
+        public async Task<ActionResult<IEnumerable<Hotel>>> GetAllHotelsByDestinationId(int destinationId)
         {
             //Maybe add some verification controls
 
             var availableHotels = await _hotelService.GetAllHotelsByDestination(destinationId);
 
-            return availableHotels;
+            if (availableHotels == null)
+            {
+                return NotFound();
+            }
+
+
+            return availableHotels.ToList();
 
         }
 
 
         [HttpGet("/api/availablehotelsByDestinationId/{destinationId}/{checkInDate}/{checkOutDate}")]
-        public async Task<IEnumerable<Hotel>> GetAllAvailableHotelsByDestinationId(int destinationId, DateTime checkInDate, DateTime checkOutDate)
+        public async Task<ActionResult<IEnumerable<Hotel>>> GetAllAvailableHotelsByDestinationId(int destinationId, DateTime checkInDate, DateTime checkOutDate)
         {
             //Maybe add some verification controls
 
             var availableHotels = await _hotelService.GetAvailableHotelsByDestinationId(destinationId, checkInDate, checkOutDate);
 
-            return availableHotels;
+            if (availableHotels == null)
+            {
+                return NotFound();
+            }
+
+            return availableHotels.ToList();
+
+        }
+
+        [HttpGet("/api/availablehotelsByCityOrCountryName/{cityOrCountryName}/{checkInDate}/{checkOutDate}")]
+        public async Task<ActionResult<IEnumerable<Hotel>>> GetAllAvailablehotelsByCityOrCountryName(string cityOrCountryName, DateTime checkInDate, DateTime checkOutDate)
+        {
+            //Maybe add some verification controls
+
+            var availableHotels = await _hotelService.GetAvailableHotelsByCityOrCountryName(cityOrCountryName, checkInDate, checkOutDate);
+
+            if (availableHotels == null)
+            {
+                return NotFound();
+            }
+
+            return availableHotels.ToList();
 
         }
 
 
-
-
         [HttpGet("/api/availablehotelsByCityName/{cityName}/{checkInDate}/{checkOutDate}")]
-        public async Task<IEnumerable<Hotel>> GetAllAvailableHotelsByCityName(string cityName, DateTime checkInDate, DateTime checkOutDate)
+        public async Task<ActionResult<IEnumerable<Hotel>>> GetAllAvailableHotelsByCityName(string cityName, DateTime checkInDate, DateTime checkOutDate)
         {
             //Maybe add some verification controls
 
             var availableHotels = await _hotelService.GetAvailableHotelsByCityName(cityName, checkInDate, checkOutDate);
 
-            return availableHotels;
+            if (availableHotels == null)
+            {
+                return NotFound();
+            }
+
+            return availableHotels.ToList();
 
         }
 
 
         [HttpGet("/api/availablehotelsByCountryName/{countryName}/{checkInDate}/{checkOutDate}")]
-        public async Task<IEnumerable<Hotel>> GetAllAvailableHotelsByCountryId(string countryName, DateTime checkInDate, DateTime checkOutDate)
+        public async Task<ActionResult<IEnumerable<Hotel>>> GetAllAvailableHotelsByCountryId(string countryName, DateTime checkInDate, DateTime checkOutDate)
         {
             //Maybe add some verification controls
 
             var availableHotels = await _hotelService.GetAvailableHotelsByCountryName(countryName, checkInDate, checkOutDate);
 
-            return availableHotels;
+            if (availableHotels == null)
+            {
+                return NotFound();
+            }
 
+            return availableHotels.ToList();
+
+        }
+
+
+        [HttpGet("/api/GetHotelImagesById/{id}")]
+        public async Task<ActionResult<IEnumerable<Hotel>>> GetHotelImagesById(int id)
+        {
+            //var hotel = await _context.Hotels.FindAsync(id).Include(x => x.HotelImages);
+
+            var hotelImages = await _hotelService.GetAllHotelImagesById(id);
+
+            if (hotelImages == null)
+            {
+                return NotFound();
+            }
+
+            return hotelImages.ToList();
+        }
+
+        [HttpGet("/api/GetHotelImagesByCityOrCountryName/{cityOrCountryName}")]
+        public async Task<ActionResult<IEnumerable<Hotel>>> GetHotelImagesByCityOrCountryName(string cityOrCountryName)
+        {
+            //var hotel = await _context.Hotels.FindAsync(id).Include(x => x.HotelImages);
+
+            var hotelImages = await _hotelService.GetHotelImagesByCityOrCountryName(cityOrCountryName);
+
+            if (hotelImages == null)
+            {
+                return NotFound();
+            }
+
+            return hotelImages.ToList();
+        }
+
+
+
+        [HttpGet("/api/GetHotelImagesByHotelName/{hotelName}")]
+        public async Task<ActionResult<IEnumerable<Hotel>>> GetHotelImagesByHotelName(string hotelName)
+        {
+            //var hotel = await _context.Hotels.FindAsync(id).Include(x => x.HotelImages);
+
+            var hotelImages = await _hotelService.GetHotelImagesByHotelName(hotelName);
+
+            if (hotelImages == null)
+            {
+                return NotFound();
+            }
+
+            return hotelImages.ToList();
         }
 
     }
--- HolidayMaker-API/HolidayMaker-API/Services/HotelService.cs
+++ HolidayMaker-API/HolidayMaker-API/Services/HotelService.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using HolidayMaker_API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using HolidayMaker_API.Models;
-using Microsoft.EntityFrameworkCore;
 
 namespace HolidayMaker_API.Services
 {
@@ -18,11 +18,44 @@ namespace HolidayMaker_API.Services
             _rs = roomService;
         }
 
+        public async Task<IEnumerable<Hotel>> GetAllHotelImagesById(int id)
+        {
+            return await _holidayMakerContext.Hotels.Where(h => h.DestinationId == id)
+                .Include(x => x.HotelImages)
+                .ToListAsync();
+        }
+
+
+
+        public async Task<IEnumerable<Hotel>> GetHotelImagesByCityOrCountryName(string searchString)
+        {
+            return await _holidayMakerContext.Hotels.Where(h => h.Destination.City.ToLower() == searchString.ToLower() ||
+                                                                     h.Destination.Country.ToLower() == searchString.ToLower())
+                                                                     .Include(x => x.HotelImages)
+                                                                     .ToListAsync();
+        }
+
+
+        public async Task<IEnumerable<Hotel>> GetHotelImagesByHotelName(string hotelName)
+        {
+            return await _holidayMakerContext.Hotels.Where(h => h.Name.ToLower() == hotelName.ToLower())
+                                                                     .Include(x => x.HotelImages)
+                                                                     .ToListAsync();
+        }
+
+
+
+
         public async Task<IEnumerable<Hotel>> GetAllHotelsByDestination(int id)
         {
-            return await _holidayMakerContext.Hotels.Where(h => h.DestinationId == id).ToListAsync();
+            return await _holidayMakerContext.Hotels.Where(h => h.DestinationId == id)
+                .Include(x => x.Rooms)
+                .ThenInclude(x => x.RoomType)
+                .ThenInclude(y => y.Rooms)
+                .ThenInclude(z => z.RoomImages)
+                .ToListAsync();
         }
-        
+
 
         public async Task<IEnumerable<Hotel>> GetAvailableHotelsByDestinationId(int destinationId, DateTime checkInDate, DateTime checkOutDate)
         {
@@ -32,12 +65,17 @@ namespace HolidayMaker_API.Services
               ((checkOutDate >= b.Booking.CheckInDate) && (checkOutDate <= b.Booking.CheckOutDate)) ||
               ((checkInDate <= b.Booking.CheckInDate) && (checkOutDate >= b.Booking.CheckInDate) && (checkOutDate <= b.Booking.CheckOutDate)) ||
               ((checkInDate >= b.Booking.CheckInDate) && (checkInDate <= b.Booking.CheckOutDate) && (checkOutDate >= b.Booking.CheckOutDate)) ||
-              ((checkInDate <= b.Booking.CheckInDate) && (checkOutDate >= b.Booking.CheckOutDate))))).ToListAsync();
+              ((checkInDate <= b.Booking.CheckInDate) && (checkOutDate >= b.Booking.CheckOutDate)))))
+                .Include(x => x.Rooms)
+                .ThenInclude(x => x.RoomType)
+                .ThenInclude(y => y.Rooms)
+                .ThenInclude(z => z.RoomImages)
+                .ToListAsync();
 
             return availableHotels;
         }
 
-        public async Task<IEnumerable<Hotel>> GetAvailableHotelsByCityName(string searchInput, DateTime checkInDate, DateTime checkOutDate)
+        public async Task<List<Hotel>> GetAvailableHotelsByCityOrCountryName(string searchInput, DateTime checkInDate, DateTime checkOutDate)
         {
             //Returnerar alla lediga hotel FUNKAR 100%
             var availableHotels = await _holidayMakerContext.Hotels.Where(d => d.Destination.City.ToLower() == searchInput.ToLower() || d.Destination.Country.ToLower() == searchInput.ToLower()).Where(r => r.Rooms.Any(x => !x.BookingXrooms.Any(b =>
@@ -45,10 +83,34 @@ namespace HolidayMaker_API.Services
               ((checkOutDate >= b.Booking.CheckInDate) && (checkOutDate <= b.Booking.CheckOutDate)) ||
               ((checkInDate <= b.Booking.CheckInDate) && (checkOutDate >= b.Booking.CheckInDate) && (checkOutDate <= b.Booking.CheckOutDate)) ||
               ((checkInDate >= b.Booking.CheckInDate) && (checkInDate <= b.Booking.CheckOutDate) && (checkOutDate >= b.Booking.CheckOutDate)) ||
-              ((checkInDate <= b.Booking.CheckInDate) && (checkOutDate >= b.Booking.CheckOutDate))))).ToListAsync();
+              ((checkInDate <= b.Booking.CheckInDate) && (checkOutDate >= b.Booking.CheckOutDate)))))
+                .Include(x => x.Rooms)
+                .ThenInclude(x => x.RoomType)
+                .ThenInclude(y => y.Rooms)
+                .ThenInclude(z => z.RoomImages)
+                .ToListAsync();
 
             return availableHotels;
         }
+
+        public async Task<IEnumerable<Hotel>> GetAvailableHotelsByCityName(string searchInput, DateTime checkInDate, DateTime checkOutDate)
+        {
+            //Returnerar alla lediga hotel FUNKAR 100%
+            var availableHotels = await _holidayMakerContext.Hotels.Where(d => d.Destination.City.ToLower() == searchInput.ToLower()).Where(r => r.Rooms.Any(x => !x.BookingXrooms.Any(b =>
+              ((checkInDate >= b.Booking.CheckInDate) && (checkInDate <= b.Booking.CheckOutDate)) ||
+              ((checkOutDate >= b.Booking.CheckInDate) && (checkOutDate <= b.Booking.CheckOutDate)) ||
+              ((checkInDate <= b.Booking.CheckInDate) && (checkOutDate >= b.Booking.CheckInDate) && (checkOutDate <= b.Booking.CheckOutDate)) ||
+              ((checkInDate >= b.Booking.CheckInDate) && (checkInDate <= b.Booking.CheckOutDate) && (checkOutDate >= b.Booking.CheckOutDate)) ||
+              ((checkInDate <= b.Booking.CheckInDate) && (checkOutDate >= b.Booking.CheckOutDate)))))
+                .Include(x => x.Rooms)
+                .ThenInclude(x => x.RoomType)
+                .ThenInclude(y => y.Rooms)
+                .ThenInclude(z => z.RoomImages)
+                .ToListAsync();
+
+            return availableHotels;
+        }
+
 
         public async Task<IEnumerable<Hotel>> GetAvailableHotelsByCountryName(string countryName, DateTime checkInDate, DateTime checkOutDate)
         {
@@ -58,7 +120,12 @@ namespace HolidayMaker_API.Services
               ((checkOutDate >= b.Booking.CheckInDate) && (checkOutDate <= b.Booking.CheckOutDate)) ||
               ((checkInDate <= b.Booking.CheckInDate) && (checkOutDate >= b.Booking.CheckInDate) && (checkOutDate <= b.Booking.CheckOutDate)) ||
               ((checkInDate >= b.Booking.CheckInDate) && (checkInDate <= b.Booking.CheckOutDate) && (checkOutDate >= b.Booking.CheckOutDate)) ||
-              ((checkInDate <= b.Booking.CheckInDate) && (checkOutDate >= b.Booking.CheckOutDate))))).ToListAsync();
+              ((checkInDate <= b.Booking.CheckInDate) && (checkOutDate >= b.Booking.CheckOutDate)))))
+                .Include(x => x.Rooms)
+                .ThenInclude(x => x.RoomType)
+                .ThenInclude(y => y.Rooms)
+                .ThenInclude(z => z.RoomImages)
+                .ToListAsync();
 
             return availableHotels;
         }
