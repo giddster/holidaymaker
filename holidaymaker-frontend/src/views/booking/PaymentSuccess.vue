11f58eeb0f2db64fd23125e93b7c12cd9b8653b7
giddster <73110741+giddster@users.noreply.github.com>
2021-06-07 09:18:31 +0200

    Update PaymentSuccess.vue
    minor design edit

--- holidaymaker-frontend/src/views/booking/PaymentSuccess.vue
+++ holidaymaker-frontend/src/views/booking/PaymentSuccess.vue
@@ -1,8 +1,8 @@
 <template>
         <div class="paymentsucceeded">
             <h1>Payment successful!</h1>
+            <h3>Booking number: XXX</h3>
             <h4>Start packing your bags!</h4>
-            <router-link to="/" class="btn btn-lg btn-primary">View booking</router-link> &nbsp; &nbsp; &nbsp;
             <router-link to="/" class="btn btn-lg btn-primary">Back to homepage</router-link>
         </div>
         
